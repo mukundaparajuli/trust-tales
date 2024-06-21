58ad6022237ab27d9de38b18c7f4c3ec3b5353b1
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-06-21 20:01:52 +0545

    integrated gemini ai to generate message suggestions

--- src/app/Api/suggest-messages/route.ts
+++ src/app/Api/suggest-messages/route.ts
@@ -1,21 +1,21 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function POST(req: Request) {
-  try {
-    const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const result = await streamText({
-      model: openai("gpt-4-turbo"),
-      prompt,
-    });
+  const prompt =
+    "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-    return result.toAIStreamResponse();
-  } catch (error) {
-    console.log("Error: ", error);
-  }
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  const text = response.text();
+  console.log(text);
+
+  const messageArray = text.split("||");
+  console.log(messageArray);
+  return Response.json({
+    messageSuggestion: messageArray,
+  });
 }
--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -23,13 +23,14 @@ import { Message } from "@/models/user";
 import { useToast } from "./ui/use-toast";
 import { ApiResponse } from "../../types/ApiResponse";
 import axios from "axios";
+import dayjs from "dayjs";
 type messageCardProps = {
   message: Message;
   onMessageDelete: (messageId: string) => void;
 };
 const MessageCard = ({ message, onMessageDelete }: messageCardProps) => {
   const { toast } = useToast();
-
+  console.log(message);
   const handleDeleteConfirm = async () => {
     const response = await axios.delete<ApiResponse>(
       `/api/delete-message/${message?._id}`
@@ -42,37 +43,36 @@ const MessageCard = ({ message, onMessageDelete }: messageCardProps) => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="destructive">
-              <X className="h-4 w-4" />
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDeleteConfirm}>
-                Continue
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-        <CardDescription>Card Description</CardDescription>
+        <div className="flex justify-between items-center">
+          <CardTitle>{message.content}</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                <X className="h-4 w-4" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        {/* <CardDescription>{message.content}</CardDescription> */}
+        <div className="text-sm">
+          {dayjs(message.createdAt).format("MMM D, YYYY h:mm A")}
+        </div>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
     </Card>
   );
 };
